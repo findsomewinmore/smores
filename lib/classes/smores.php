72fdd2b092386416cf81dca3f97b6499f99c4fca
Connor McKelvey <connormckelvey@gmail.com>
2015-01-05 16:33:09 -0500

    Update smores.php

--- lib/classes/smores.php
+++ lib/classes/smores.php
@@ -17,15 +17,56 @@ class smores {
 	  'respond'   => '/assets/js/vendor/respond.min.js'
 	);
 
- 	/**
-	 * Call this method to return the file path of an asset.
-	 * Ex: smores::get_asset('css');
-	 * @since Smores 1.1
-	 * @param string $asset, the key of within the assets array.
-	 * @return string, the value index $asset
-	 */
-	public static function get_asset($asset){
-	  return self::$assets[$asset];
+  /**
+  * Call this method to return the file path of an asset.
+  * Ex: smores::get_asset('css');
+  * @since Smores 1.1
+  * @param string $asset, the key of within the assets array.
+  * @return string, the value index $asset
+  */
+  public static function get_asset($asset){
+    return self::$assets[$asset];
+  }
+    //Utility Functions
 
-	}
+  /**
+   * smores_files_in_dir
+   *
+   * Loops through a directory <$dir> and returns an array of files
+   * in that directory
+   *
+   * @param (string) ($dir) path relative to wp-content
+   * @param (array) ($ignores) files to ignore, default: init.php
+   * @return (array) an array for files relative to wp-content
+   */
+  public static function files_in_dir($dir, $ignores = array('init.php')) {
+    $smores_include_files = array();
+    foreach(glob(SMORES_THEME_ROOT."/${dir}/*.php") as $filename){
+      $paths = explode('/', $filename);
+      $file = end($paths);
+      foreach($ignores as $ignore){
+        if($file !== $ignore)
+          $smores_include_files[] = "${dir}/${file}";
+      }
+    }
+    return $smores_include_files;
+  }
+
+  /**
+   * smores_include_array
+   *
+   * Loops through a array <$array> and includes the files
+   *
+   * @param (array) ($array)
+   * @return nothing
+   */
+  public static function include_array($includes_array) {
+    foreach ($includes_array as $file) {
+      if (!$filepath = locate_template($file)) {
+        trigger_error(sprintf('Error locating %s for inclusion', $file), E_USER_ERROR);
+      }
+      require_once $filepath;
+    }
+    unset($file, $filepath);
+  }
 }
