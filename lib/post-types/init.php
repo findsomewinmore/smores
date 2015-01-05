e1ab83eb5c5d959005d2f89f6bd08aeb0813cf6e
Connor McKelvey <connormckelvey@gmail.com>
2015-01-05 16:34:12 -0500

    Update init.php

--- lib/post-types/init.php
+++ lib/post-types/init.php
@@ -1,4 +1,4 @@
 <?php
-  $smores_post_types = smores_files_in_dir('lib/post-types');
-  smores_include_array($smores_post_types);
+  $smores_post_types = smores::files_in_dir('lib/post-types');
+  smores::include_array($smores_post_types);
 ?>
