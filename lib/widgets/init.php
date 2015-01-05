8ff1b2ee1c9ef73592ae3c25640be751f5724b6c
Connor McKelvey <connormckelvey@gmail.com>
2015-01-05 16:34:30 -0500

    Update init.php

--- lib/widgets/init.php
+++ lib/widgets/init.php
@@ -1,4 +1,4 @@
 <?php
-  $smores_widgets = smores_files_in_dir('lib/widgets');
-  smores_include_array($smores_widgets);
+  $smores_widgets = smores::files_in_dir('lib/widgets');
+  smores::include_array($smores_widgets);
 ?>
